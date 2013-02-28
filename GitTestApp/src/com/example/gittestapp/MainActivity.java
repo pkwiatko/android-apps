de324a78f7d5fa27856fa9b0e8f071c01e17c389
pkwiatko <kwiatp@gmail.com>
2013-02-28 14:41:09 +0100

    test1

--- GitTestApp/src/com/example/gittestapp/MainActivity.java
+++ GitTestApp/src/com/example/gittestapp/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	//
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
     }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
